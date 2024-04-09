c00f45b55548daba1407ac0fc66b9902f36a0da8
sasha1107 <sasha1107@naver.com>
2024-04-10 00:53:29 +0900

    fix: 로딩 컴포넌트 스타일 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,10 @@ import Spinner from "@/assets/spinner.svg?react";
 const Loading = () => {
   return (
     <div className="w-dvw h-dvh flex justify-center items-center fixed top-0 left-0 z-50 bg-white-dimmed">
-      <div role="status">
+      <div
+        role="status"
+        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-400 fill-primary-400"
+      >
         <Spinner />
         <span className="sr-only">Loading...</span>
       </div>

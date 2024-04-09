afa2805370513e5743469de64c07d5611aadff58
sasha1107 <sasha1107@naver.com>
2024-04-10 00:53:42 +0900

    feat: 주문 페이지 구현중

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
 const Order = () => {
+  const location = useLocation();
+  // const setSelectedCart = useSetAtom(selectedCartAtom);
+  console.log(location.state);
+
   return <div>Order</div>;
 };
 
